129b4ead4199748275f4eebb53d18f00af4dc320
palkan14 <87696017+palkan14@users.noreply.github.com>
2021-10-24 14:13:29 +0530

    College Data
    id for the given colleges will remain same in every jsx file

---
+++ src/components/college/CollegeCardData.jsx
@@ -0,0 +1,81 @@
+const data = [
+    {
+        id: "1",
+        name:"Maharaja Sayajirao University",
+        fees:"₹ 7,740-BA",
+        city:"Vadodara",
+        src=""
+    },
+    {
+        id: "2",
+        name:"Navrachna University",
+        fees:"",
+        city:"Vadodara",
+        src=""
+    },
+    {
+        id: "3",
+        name:"Parul University",
+        fees:"₹100,000-BE/B.TECH",
+        city:"Vadodara",
+        src=""
+    },
+    {
+        id: "4",
+        name:"Nirma University",
+        fees:"₹ 197,200-BE/B.TECH",
+        city:"Ahemdabad",
+        src=""
+    },
+    {
+        id: "5",
+        name:"DDU",
+        fees:"",
+        city:"Nadiad",
+        src=""
+    },
+    {
+        id: "6",
+        name:"CEPT University",
+        fees:"₹ 378,000-M.ARCH ",
+        city:"Ahemdabad",
+        src=""
+    },
+    {
+        id: "7",
+        name:"Pandit Deendayal Petroleum University [PDPU]/(Energy University - PDEU)",
+        fees:"₹ 253,000-BE/B.TECH",
+        city:"Gandhinager",
+        src=""
+    },
+    {
+        id: "8",
+        name:"Indian School Of Business Management And Administration - [ISBM]",
+        fees:"₹ 14,900-GRADUATE DIPLOMA IN MANAGEMENT ",
+        city:"Ahemdabad",
+        src=""
+    },
+    {
+        id: "9",
+        name:"AURO University",
+        fees:"₹ 290,000-BBA/BBM ",
+        city:"Surat",
+        src=""
+    },
+    {
+        id: "10",
+        name:"L.D. College Of Engineering - [LDCE]",
+        fees:"₹ 2,860-BE/B.TECH",
+        city:"Ahemdabad",
+        src=""
+    },
+    {
+        id: "11",
+        name:"RK University",
+        fees:"₹ 76,500-BE/B.TECH",
+        city:"Rajkot",
+        src=""
+    }
+]
+
+export default data;
