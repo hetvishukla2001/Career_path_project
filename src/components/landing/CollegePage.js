d16f50a522ac7bf2f0e9e8c426fdea799e799859
palkan14 <87696017+palkan14@users.noreply.github.com>
2021-11-13 14:22:46 +0530

    updated college page

--- src/components/landing/CollegePage.js
+++ src/components/landing/CollegePage.js
@@ -110,37 +110,178 @@ const CollegePage = () => {
                     </p>
                 </TabPane>
                 <TabPane active={openTab === 2 ? true : false}>
-                    <p>
-                        I will be the leader of a company that ends up being worth
-                        billions of dollars, because I got the answers. I understand
-                        culture. I am the nucleus. I think that’s a responsibility that
-                        I have, to push possibilities, to show people, this is the level
-                        that things could be at. I think that’s a responsibility that I
-                        have, to push possibilities, to show people, this is the level
-                        that things could be at.
-                    </p>
+                    <h2 style={{textAlign:"left",fontSize:"24px",fontWeight:"bold",paddingBottom:"25px"}}>Courses & Fees</h2>
+                    <div style={{paddingLeft:"120px"}}>
+                        <table style={{width:"700px"}}>
+                            <thead className="table1">
+                                <tr className="table1">
+                                    <th>Courses</th>
+                                    <th>Fees</th>
+                                    <th>Eligibility</th>
+                                </tr>
+                            </thead>
+                            <tbody className="table1">
+                                <tr className="table1">
+                                    <td>{data[0].course1.name}</td>
+                                    <td>{data[0].course1.fees}</td>
+                                    <td>{data[0].course1.eli}</td>
+                                </tr>
+                                <tr className="table1">
+                                    <td>{data[0].course2.name}</td>
+                                    <td>{data[0].course2.fees}</td>
+                                    <td>{data[0].course2.eli}</td>
+                                </tr>
+                                <tr className="table1">
+                                    <td>{data[0].course3.name}</td>
+                                    <td>{data[0].course3.fees}</td>
+                                    <td>{data[0].course3.eli}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </TabPane>
                 <TabPane active={openTab === 3 ? true : false}>
-                    <p>
-                        I think that’s a responsibility that I have, to push
-                        possibilities, to show people, this is the level that things
-                        could be at. So when you get something that has the name Kanye
-                        West on it, it’s supposed to be pushing the furthest
-                        possibilities. I will be the leader of a company that ends up
-                        being worth billions of dollars, because I got the answers. I
-                        understand culture. I am the nucleus.
-                    </p>
+                    {/* Gallery */}
+                    <h2 style={{textAlign:"left",fontSize:"24px",fontWeight:"bold",paddingBottom:"50px"}}>Gallery</h2>
+                    <div className="flex flex-wrap -mt-12 justify-center">
+                        <Image
+                            src="data1[0].src1"//how to do this
+                            rounded={false}
+                            raised={true}
+                            alt="Raised Image"
+                            className="text-blue-gray-800 p-3 w-2 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center mb-6"
+                        />
+                        <Image
+                            src="https://images.static-collegedunia.com/public/college_data/images/campusimage/142181816320130902_153619.jpg?tr=h-135,w-205,c-force"
+                            rounded={false}
+                            raised={true}
+                            alt="Raised Image"
+                            className="text-blue-gray-800 p-3 w-2 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center mb-6"
+                        />
+                        <Image
+                            src="https://images.static-collegedunia.com/public/college_data/images/campusimage/142181816320130902_153619.jpg?tr=h-135,w-205,c-force"
+                            rounded={false}
+                            raised={true}
+                            alt="Raised Image"
+                            className="text-blue-gray-800 p-3 w-2 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center mb-6"
+                        />
+                        <Image
+                            src="https://images.static-collegedunia.com/public/college_data/images/campusimage/142181816320130902_153619.jpg?tr=h-135,w-205,c-force"
+                            rounded={false}
+                            raised={true}
+                            alt="Raised Image"
+                            className="text-blue-gray-800 p-3 w-2 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center mb-6"
+                        />
+                        <Image
+                            src="https://images.static-collegedunia.com/public/college_data/images/campusimage/142181816320130902_153619.jpg?tr=h-135,w-205,c-force"
+                            rounded={false}
+                            raised={true}
+                            alt="Raised Image"
+                            className="text-blue-gray-800 p-3 w-2 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center mb-6"
+                        />
+                        <Image
+                            src="https://images.static-collegedunia.com/public/college_data/images/campusimage/142181816320130902_153619.jpg?tr=h-135,w-205,c-force"
+                            rounded={false}
+                            raised={true}
+                            alt="Raised Image"
+                            className="text-blue-gray-800 p-3 w-2 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center mb-6"
+                        />
+                    </div>
+                    <div className="flex flex-wrap -mt-12 justify-center">
+                        <Image
+                            src="https://images.static-collegedunia.com/public/college_data/images/campusimage/142181816320130902_153619.jpg?tr=h-135,w-205,c-force"
+                            rounded={false}
+                            raised={true}
+                            alt="Raised Image"
+                            className="text-blue-gray-800 p-3 w-2 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center mb-6"
+                        />
+                        <Image
+                            src="https://images.static-collegedunia.com/public/college_data/images/campusimage/142181816320130902_153619.jpg?tr=h-135,w-205,c-force"
+                            rounded={false}
+                            raised={true}
+                            alt="Raised Image"
+                            className="text-blue-gray-800 p-3 w-2 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center mb-6"
+                        />
+                        <Image
+                            src="https://images.static-collegedunia.com/public/college_data/images/campusimage/142181816320130902_153619.jpg?tr=h-135,w-205,c-force"
+                            rounded={false}
+                            raised={true}
+                            alt="Raised Image"
+                            className="text-blue-gray-800 p-3 w-2 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center mb-6"
+                        />
+                        <Image
+                            src="https://images.static-collegedunia.com/public/college_data/images/campusimage/142181816320130902_153619.jpg?tr=h-135,w-205,c-force"
+                            rounded={false}
+                            raised={true}
+                            alt="Raised Image"
+                            className="text-blue-gray-800 p-3 w-2 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center mb-6"
+                        />
+                        <Image
+                            src="https://images.static-collegedunia.com/public/college_data/images/campusimage/142181816320130902_153619.jpg?tr=h-135,w-205,c-force"
+                            rounded={false}
+                            raised={true}
+                            alt="Raised Image"
+                            className="text-blue-gray-800 p-3 w-2 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center mb-6"
+                        />
+                        <Image
+                            src="https://images.static-collegedunia.com/public/college_data/images/campusimage/142181816320130902_153619.jpg?tr=h-135,w-205,c-force"
+                            rounded={false}
+                            raised={true}
+                            alt="Raised Image"
+                            className="text-blue-gray-800 p-3 w-2 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center mb-6"
+                        />
+                    </div>
                 </TabPane>
                 <TabPane active={openTab === 4 ? true : false}>
-                    <p>
-                        I think that’s a responsibility that I have, to push
-                        possibilities, to show people, this is the level that things
-                        could be at. So when you get something that has the name Kanye
-                        West on it, it’s supposed to be pushing the furthest
-                        possibilities. I will be the leader of a company that ends up
-                        being worth billions of dollars, because I got the answers. I
-                        understand culture. I am the nucleus.
-                    </p>
+                    {/* Faculty */}
+                    <h2 style={{textAlign:"left",fontSize:"24px",fontWeight:"bold",paddingBottom:"25px"}}>Faculty</h2>
+                            <div style={{textAlign:"left",display: "flex",flexWrap:"wrap",margin: "0px -8px",paddingLeft: "120px"}}>
+                                <div style={{textAlign:"left",position: "relative",flex: "0 0 30%",marginBottom: "1rem",paddingRight: ".5rem",paddingLeft: ".5rem"}}>
+                                    <p>Faculty name</p>
+                                    <p>Faculty position</p>
+                                    <p>Faculty contact number</p>
+                                </div>
+                                <div style={{textAlign:"left",position: "relative",flex: "0 0 30%",marginBottom: "1rem",paddingRight: ".5rem",paddingLeft: ".5rem"}}>
+                                    <p>Faculty name</p>
+                                    <p>Faculty position</p>
+                                    <p>Faculty contact number</p>
+                                </div>
+                                <div style={{textAlign:"left",position: "relative",flex: "0 0 30%",marginBottom: "1rem",paddingRight: ".5rem",paddingLeft: ".5rem"}}>
+                                    <p>Faculty name</p>
+                                    <p>Faculty position</p>
+                                    <p>Faculty contact number</p>
+                                </div>
+                                <div style={{textAlign:"left",position: "relative",flex: "0 0 30%",marginBottom: "1rem",paddingRight: ".5rem",paddingLeft: ".5rem"}}>
+                                    <p>Faculty name</p>
+                                    <p>Faculty position</p>
+                                    <p>Faculty contact number</p>
+                                </div>
+                                <div style={{textAlign:"left",position: "relative",flex: "0 0 30%",marginBottom: "1rem",paddingRight: ".5rem",paddingLeft: ".5rem"}}>
+                                    <p>Faculty name</p>
+                                    <p>Faculty position</p>
+                                    <p>Faculty contact number</p>
+                                </div>
+                                <div style={{textAlign:"left",position: "relative",flex: "0 0 30%",marginBottom: "1rem",paddingRight: ".5rem",paddingLeft: ".5rem"}}>
+                                    <p>Faculty name</p>
+                                    <p>Faculty position</p>
+                                    <p>Faculty contact number</p>
+                                </div>
+                                <div style={{textAlign:"left",position: "relative",flex: "0 0 30%",marginBottom: "1rem",paddingRight: ".5rem",paddingLeft: ".5rem"}}>
+                                    <p>Faculty name</p>
+                                    <p>Faculty position</p>
+                                    <p>Faculty contact number</p>
+                                </div>
+                                <div style={{textAlign:"left",position: "relative",flex: "0 0 30%",marginBottom: "1rem",paddingRight: ".5rem",paddingLeft: ".5rem"}}>
+                                    <p>Faculty name</p>
+                                    <p>Faculty position</p>
+                                    <p>Faculty contact number</p>
+                                </div>
+                                <div style={{textAlign:"left",position: "relative",flex: "0 0 30%",marginBottom: "1rem",paddingRight: ".5rem",paddingLeft: ".5rem"}}>
+                                    <p>Faculty name</p>
+                                    <p>Faculty position</p>
+                                    <p>Faculty contact number</p>
+                                </div>
+                            </div>
                 </TabPane>
                 <TabPane active={openTab === 5 ? true : false}>
                     <p>
@@ -154,15 +295,66 @@ const CollegePage = () => {
                     </p>
                 </TabPane>
                 <TabPane active={openTab === 6 ? true : false}>
-                    <p>
-                        I think that’s a responsibility that I have, to push
-                        possibilities, to show people, this is the level that things
-                        could be at. So when you get something that has the name Kanye
-                        West on it, it’s supposed to be pushing the furthest
-                        possibilities. I will be the leader of a company that ends up
-                        being worth billions of dollars, because I got the answers. I
-                        understand culture. I am the nucleus.
-                    </p>
+                    {/* Hostel */}
+                    <h2 style={{textAlign:"left",fontSize:"24px",fontWeight:"bold",paddingBottom:"25px"}}>Hostel for Female</h2>
+                    <div style={{paddingLeft:"120px"}}>
+                        <table className=" table table-striped border-1 table1"style={{width:"700px"}}>
+                            <thead className="table1">
+                                <tr className="table1">
+                                    <th>Sr. No.</th>
+                                    <th>Name of Hostel</th>
+                                    <th>Capacity of Hostel</th>
+                                </tr>
+                            </thead>
+                            <tbody className="table1">
+                                <tr className="table1">
+                                    <td>c1</td>
+                                    <td>f1</td>
+                                    <td>e1</td>
+                                </tr>
+                                <tr className="table1">
+                                    <td>c1</td>
+                                    <td>f1</td>
+                                    <td>e1</td>
+                                </tr>
+                                <tr className="table1">
+                                    <td>c1</td>
+                                    <td>f1</td>
+                                    <td>e1</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <h2 style={{textAlign:"left",fontSize:"24px",fontWeight:"bold",paddingBottom:"25px"}}>Hostel for Male</h2>
+                    <div style={{paddingLeft:"120px"}}>
+                        <table className=" table table-striped border-1 table1"style={{width:"700px"}}>
+                            <thead className="table1">
+                                <tr className="table1">
+                                    <th>Sr. No.</th>
+                                    <th>Name of Hostel</th>
+                                    <th>Capacity of Hostel</th>
+                                </tr>
+                            </thead>
+                            <tbody className="table1">
+                                <tr className="table1">
+                                    <td>c1</td>
+                                    <td>f1</td>
+                                    <td>e1</td>
+                                </tr>
+                                <tr className="table1">
+                                    <td>c1</td>
+                                    <td>f1</td>
+                                    <td>e1</td>
+                                </tr>
+                                <tr className="table1">
+                                    <td>c1</td>
+                                    <td>f1</td>
+                                    <td>e1</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </TabPane>
                 <TabPane active={openTab === 7 ? true : false}>
                     <p>
