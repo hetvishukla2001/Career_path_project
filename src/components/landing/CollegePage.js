aa1ebb1eb2779d8902790823f48e46a363c79dbb
palkan14 <87696017+palkan14@users.noreply.github.com>
2021-11-13 14:14:18 +0530

    College page

--- src/components/landing/CollegePage.js
+++ src/components/landing/CollegePage.js
@@ -1,154 +1,180 @@
-import React from 'react';
 // import './CollegenameApi';
-// import './CourseName';
-import './CollegePage.css';
+// // import './CourseName';
+// import './CollegePage.css';
+import Tab from "@material-tailwind/react/Tab";
+import TabList from "@material-tailwind/react/TabList";
+import TabItem from "@material-tailwind/react/TabItem";
+import TabContent from "@material-tailwind/react/TabContent";
+import TabPane from "@material-tailwind/react/TabPane";
 
 import data from '../../school.jsx';
 
 const CollegePage = () => { 
-    // name, coursename, fees, eligibility
+    const [openTab, setOpenTab] = useState(1);
+
     return (
-        <>
 
-        <nav className="nav tab-wrapper position-relative bg-white false">
-            <ul className="ul1 list-unstyled position-relative" >
-                <li className="li1 position-relative nav-item hover1" onClick={() => window.location.replace("/#info")} >
+        <Tab>
+            <TabList color="lightBlue">
+                <TabItem
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setOpenTab(1);
+                    }}
+                    ripple="light"
+                    active={openTab === 1 ? true : false}
+                    href="tabItem"
+                >
                     Info
-                </li>
-                <li className="li1 position-relative nav-item hover1" onClick={() => window.location.replace("/#courses")} >
-                    Courses & Fees
-                </li>
-                <li className="li1 position-relative nav-item hover1" onClick={() => window.location.replace("/#reviews")} >
-                    Reviews
-                </li>
-                <li className="li1 position-relative nav-item hover1" onClick={() => window.location.replace("/#placement")} >
-                    Placement
-                </li>
-                <li className="li1 position-relative nav-item hover1" onClick={() => window.location.replace("/#gallery")} >
-                    Gallery
-                </li>
-                <li className="li1 position-relative nav-item hover1" onClick={() => window.location.replace("/#faculty")} >
+                </TabItem>
+                <TabItem
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setOpenTab(2);
+                    }}
+                    ripple="light"
+                    active={openTab === 2 ? true : false}
+                    href="tabItem"
+                >
+                    Course & Fees
+                </TabItem>
+                <TabItem
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setOpenTab(3);
+                    }}
+                    ripple="light"
+                    active={openTab === 3 ? true : false}
+                    href="tabItem"
+                >
                     Faculty
-                </li>
-                <li className="li1 position-relative nav-item hover1" onClick={() => window.location.replace("/#hostel")} >
+                </TabItem>
+                <TabItem
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setOpenTab(4);
+                    }}
+                    ripple="light"
+                    active={openTab === 4 ? true : false}
+                    href="tabItem"
+                >
+                    Faculty
+                </TabItem>
+                <TabItem
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setOpenTab(5);
+                    }}
+                    ripple="light"
+                    active={openTab === 5 ? true : false}
+                    href="tabItem"
+                >
+                    Placement
+                </TabItem>
+                <TabItem
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setOpenTab(6);
+                    }}
+                    ripple="light"
+                    active={openTab === 6 ? true : false}
+                    href="tabItem"
+                >
                     Hostel
-                </li>
-            </ul>
-        </nav>  
+                </TabItem>
+                <TabItem
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setOpenTab(7);
+                    }}
+                    ripple="light"
+                    active={openTab === 7 ? true : false}
+                    href="tabItem"
+                >
+                    Reviews
+                </TabItem>
+            </TabList>
 
-        {/* Courses & Fees */}
-        <h2 id="courses" style={{textAlign:"left",paddingLeft:"10em"}}>Courses & Fees</h2>
-        <div style={{paddingLeft:"120px"}}>
-            <table className=" table table-striped border-1 table1"style={{width:"700px"}}>
-                <thead className="table1">
-                    <tr className="table1">
-                        <th>Course</th>
-                        <th>Fees</th>
-                        <th>Eligibility</th>
-                    </tr>
-                </thead>
-                <tbody className="table1">
-                    <tr className="table1">
-                        <td>{data[0].course1.name}</td>
-                        <td>{data[0].course1.fees}</td>
-                        <td>{data[0].course1.eli}</td>
-                    </tr>
-                    <tr className="table1">
-                        <td>{data[0].course2.name}</td>
-                        <td>f1</td>
-                        <td>e1</td>
-                    </tr>
-                    <tr className="table1">
-                        <td>{data[0].course3.name}</td>
-                        <td>f1</td>
-                        <td>e1</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-
-        {/* Gallery */}
-        <h2 style={{textAlign:"left",paddingLeft:"10em"}} id="gallery">Gallery</h2>
-        <div className="div1 row container-fluid">
-            <div className="div2 col-3">
-                {/* <img src={usa} style={{width:"200px",height:"200px"}}/> */}
-            </div>
-            <div className="div2 col-3">
-                {/* <img src={usa} style={{width:"200px",height:"200px"}}/> */}
-            </div>
-            <div className="div2 col-3">
-                {/* <img src={usa} style={{width:"200px",height:"200px"}}/> */}
-            </div>
-            <div className="div2 col-3">
-                {/* <img src={usa} style={{width:"200px",height:"200px"}}/> */}
-            </div>
-            <div className="div2 col-3">
-                {/* <img src={usa} style={{width:"200px",height:"200px"}}/> */}
-            </div>
-            <div className="div2 col-3">
-                {/* <img src={usa} style={{width:"200px",height:"200px"}}/> */}
-            </div>
-            <div className="div2 col-3">
-                {/* <img src={usa} style={{width:"200px",height:"200px"}}/> */}
-            </div>
-            <div className="div2 col-3">
-                {/* <img src={usa} style={{width:"200px",height:"200px"}}/> */}
-            </div>
-        </div>
-
-
-        {/* Faculty */}
-        <h2 style={{textAlign:"left",paddingLeft:"10em"}} id="faculty">Faculty</h2>
-        <div className="div1">
-            <div className="div3">
-                <p>Faculty name</p>
-                <p>Faculty position</p>
-                <p>Faculty contact number</p>
-            </div>
-            <div className="div3">
-                <p>Faculty name</p>
-                <p>Faculty position</p>
-                <p>Faculty contact number</p>
-            </div>
-            <div className="div3">
-                <p>Faculty name</p>
-                <p>Faculty position</p>
-                <p>Faculty contact number</p>
-            </div>
-        </div>
-
-        {/* Hostel */}
-        <h2 id="hostel" style={{textAlign:"left",paddingLeft:"10em"}}>Hostel</h2>
-        <div style={{paddingLeft:"120px"}}>
-            <table className=" table table-striped border-1 table1"style={{width:"700px"}}>
-                <thead className="table1">
-                    <tr className="table1">
-                        <th>Sr. No.</th>
-                        <th>Name of Hostel</th>
-                        <th>Capacity of Hostel</th>
-                    </tr>
-                </thead>
-                <tbody className="table1">
-                    <tr className="table1">
-                        <td>c1</td>
-                        <td>f1</td>
-                        <td>e1</td>
-                    </tr>
-                    <tr className="table1">
-                        <td>c1</td>
-                        <td>f1</td>
-                        <td>e1</td>
-                    </tr>
-                    <tr className="table1">
-                        <td>c1</td>
-                        <td>f1</td>
-                        <td>e1</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-
-        </>
+            <TabContent>
+                <TabPane active={openTab === 1 ? true : false}>
+                    <p>
+                        I think that’s a responsibility that I have, to push
+                        possibilities, to show people, this is the level that things
+                        could be at. So when you get something that has the name Kanye
+                        West on it, it’s supposed to be pushing the furthest
+                        possibilities. I will be the leader of a company that ends up
+                        being worth billions of dollars, because I got the answers. I
+                        understand culture. I am the nucleus.
+                    </p>
+                </TabPane>
+                <TabPane active={openTab === 2 ? true : false}>
+                    <p>
+                        I will be the leader of a company that ends up being worth
+                        billions of dollars, because I got the answers. I understand
+                        culture. I am the nucleus. I think that’s a responsibility that
+                        I have, to push possibilities, to show people, this is the level
+                        that things could be at. I think that’s a responsibility that I
+                        have, to push possibilities, to show people, this is the level
+                        that things could be at.
+                    </p>
+                </TabPane>
+                <TabPane active={openTab === 3 ? true : false}>
+                    <p>
+                        I think that’s a responsibility that I have, to push
+                        possibilities, to show people, this is the level that things
+                        could be at. So when you get something that has the name Kanye
+                        West on it, it’s supposed to be pushing the furthest
+                        possibilities. I will be the leader of a company that ends up
+                        being worth billions of dollars, because I got the answers. I
+                        understand culture. I am the nucleus.
+                    </p>
+                </TabPane>
+                <TabPane active={openTab === 4 ? true : false}>
+                    <p>
+                        I think that’s a responsibility that I have, to push
+                        possibilities, to show people, this is the level that things
+                        could be at. So when you get something that has the name Kanye
+                        West on it, it’s supposed to be pushing the furthest
+                        possibilities. I will be the leader of a company that ends up
+                        being worth billions of dollars, because I got the answers. I
+                        understand culture. I am the nucleus.
+                    </p>
+                </TabPane>
+                <TabPane active={openTab === 5 ? true : false}>
+                    <p>
+                        I think that’s a responsibility that I have, to push
+                        possibilities, to show people, this is the level that things
+                        could be at. So when you get something that has the name Kanye
+                        West on it, it’s supposed to be pushing the furthest
+                        possibilities. I will be the leader of a company that ends up
+                        being worth billions of dollars, because I got the answers. I
+                        understand culture. I am the nucleus.
+                    </p>
+                </TabPane>
+                <TabPane active={openTab === 6 ? true : false}>
+                    <p>
+                        I think that’s a responsibility that I have, to push
+                        possibilities, to show people, this is the level that things
+                        could be at. So when you get something that has the name Kanye
+                        West on it, it’s supposed to be pushing the furthest
+                        possibilities. I will be the leader of a company that ends up
+                        being worth billions of dollars, because I got the answers. I
+                        understand culture. I am the nucleus.
+                    </p>
+                </TabPane>
+                <TabPane active={openTab === 7 ? true : false}>
+                    <p>
+                        I think that’s a responsibility that I have, to push
+                        possibilities, to show people, this is the level that things
+                        could be at. So when you get something that has the name Kanye
+                        West on it, it’s supposed to be pushing the furthest
+                        possibilities. I will be the leader of a company that ends up
+                        being worth billions of dollars, because I got the answers. I
+                        understand culture. I am the nucleus.
+                    </p>
+                </TabPane>
+            </TabContent>
+        </Tab>
     );
 }
 
