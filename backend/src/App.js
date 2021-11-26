733905a722faf7f2ab8c57349a28534394de5375
hetvishukla2001 <64137184+hetvishukla2001@users.noreply.github.com>
2021-11-27 01:52:11 +0530

    last

--- backend/src/App.js
+++ backend/src/App.js
@@ -6,7 +6,7 @@ const express=require("express");
 const app=express();
 dotenv.config({path: 'config.env' });
 require("./db/conne")
-const port=process.env.PORT;
+const port=process.env.PORT || 5000;
 const User=require('./models/user')//import model schema
 app.use(express.json());//data is added in json format but our application doesn't understand json
 //so above middleware is used so that the json format is converted into objects and used
